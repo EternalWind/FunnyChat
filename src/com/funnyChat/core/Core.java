f05ef9266e6aaab140498118a715092dfbf5fa0a
EternalWind <eternalwind@live.cn>
2011-11-28 23:37:57 +0800

    Updates

--- src/com/funnyChat/core/Core.java
+++ src/com/funnyChat/core/Core.java
@@ -1,51 +1,68 @@
 package com.funnyChat.core;
 
-//import com.funnyChat.Thread.ThreadManager;
+import java.io.IOException;
+
 import com.funnyChat.event.EventManager;
 import com.funnyChat.memory.MemoryManager;
 import com.funnyChat.network.NetworkManager;
 import com.funnyChat.plugin.PluginManager;
-import com.funnyChat.utils.*;
+import com.funnyChat.utils.ConfigurationInfo;
+import com.funnyChat.utils.Log;
 import com.funnyChat.utils.Log.LogType;
 
 public class Core {
 
-	static private MainWindow mMainWnd = null;
+	static private MainWindow mMainWnd;
+	private ConfigurationInfo mConfInfo;
 	static private Log mLogger = null;
 	static private Core mInstance = null;
 	static private String mDEAFAULTLOGPATH = "Log.txt";
 
-	private Core() {}
-	
-	static public void initialize(String _log_path) {
-		if(mInstance == null){
-			//ThreadManager.initialize();
+	public boolean initialize(String _log_path) {
+		if (mInstance == null) {
+			// ThreadManager.initialize();
 			mInstance = new Core();
 			mLogger = new Log();
-			if(_log_path == null) {
+			if (_log_path == null) {
 				_log_path = mDEAFAULTLOGPATH;
 			}
-			mLogger.setLogFile(_log_path);
-			if(mMainWnd == null){
-				mMainWnd = new MainWindow();
+			mConfInfo = new ConfigurationInfo();
+			mMainWnd = new MainWindow();
+
+			try {
+				if (!mConfInfo.loadConfFile()) {
+					mConfInfo.createConfFile();
+					mConfInfo.loadConfFile();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+			String _default_plugin = null;
+			if ((_default_plugin = mConfInfo.getDefaultPlugins()) == null)
+				return false;
+			PluginManager.initialize(_default_plugin);
+			mMainWnd.initWindow("FunnyChat", mConfInfo);
+
 			MemoryManager.initialize();
 			EventManager.initialize();
 			NetworkManager.initialize();
-			PluginManager.initialize();
+		} else {
+			mLogger.addLog("Duplicative initialization for the Core.",
+					LogType.WARNING);
 		}
-		else {
-			mLogger.addLog("Duplicative initialization for the Core.", LogType.WARNING);
-		}
+		return true;
 	}
 
 	public void deinitialize() {
-		//ThreadManager.getInstance().deinitialize();
-		mMainWnd = null;
+		try {
+			mMainWnd.deinitWindow();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		PluginManager.getInstance().deinitialize();
 		MemoryManager.getInstance().deinitialize();
 		EventManager.getInstance().deinitialize();
 		NetworkManager.getInstance().deinitialize();
-		PluginManager.getInstance().deinitialize();
 		mLogger.saveLog();
 		mInstance = null;
 		mLogger = null;
@@ -55,27 +72,23 @@ public class Core {
 		EventManager.getInstance().start();
 		NetworkManager.getInstance().start();
 		PluginManager.getInstance().enableAll();
+		mMainWnd.run();
 	}
 
-	static public boolean registerMainWnd(MainWindow _mainWnd) {
-		if (_mainWnd != null) {
-			mMainWnd = _mainWnd;
-			return true;
-		} 
-		else {
-			mLogger.addLog("Try to register a null pointer as the main window.", LogType.DEBUG);
-			return false;
-		}
-	}
-	
+	// Abort
+	/*
+	 * public boolean registerMainWnd(MainWindow _mainWnd) { if (_mainWnd !=
+	 * null) { mMainWnd = _mainWnd; return true; } else return false; }
+	 */
+
 	static public MainWindow getMainWindow() {
 		return mMainWnd;
 	}
-	
+
 	static public Core getInstance() {
 		return mInstance;
 	}
-	
+
 	static public Log getLogger() {
 		return mLogger;
 	}
