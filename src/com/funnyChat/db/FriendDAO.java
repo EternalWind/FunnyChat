c35bccd625c096a2684be602d46714f5dae68f1c
feng2238 <1141952871@qq.com>
2011-12-01 23:19:40 +0800

    db是与数据库操作部分 server是服务器插件 MessageEvent是客户端与服务器通信的通用事件

---
+++ src/com/funnyChat/db/FriendDAO.java
@@ -0,0 +1,72 @@
+package com.funnyChat.db;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class FriendDAO {
+	private Statement stmt;
+
+	public FriendDAO() {
+		stmt = Conn.getInstance().getStmt();
+	}
+
+	public ArrayList<Long> find(long uid) {
+		ArrayList<Long> friendsList = new ArrayList<Long>();
+		try {
+			String sql = "select * from friend where uid1 = " + uid
+					+ " or uid2 = " + uid;
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				Long l1 = rs.getLong(2);
+				Long l2 = rs.getLong(3);
+				if (l1.equals(uid))
+					friendsList.add(l2);
+				else
+					friendsList.add(l1);
+				System.out.println("l1:"+l1+" l2:"+l2);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return friendsList;
+	}
+	public boolean add(long uid1, long uid2) {
+		try {
+			String sql = "insert into friend(uid1,uid2) values("+ uid1
+					+ " , " + uid2 +")";
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	public boolean delete(long fid) {
+		try {
+			String sql = "delete from friend where fid = "+fid;
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean delete(Long uid1, Long uid2) {
+		try {
+			String sql = "delete from friend where uid1 = "+uid1 +" and uid2 = "+ uid2;
+			stmt.execute(sql);
+			sql = "delete from friend where uid1 = "+uid2 +" and uid2 = "+ uid1;
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	public static void main(String[] args){
+		FriendDAO friendDAO = new FriendDAO();
+		friendDAO.add(1, 6);
+		friendDAO.find(1);
+		friendDAO.delete(new Long(1), new Long(5));
+		friendDAO.find(1);
+	}
+}
---
+++ src/com/funnyChat/db/PluginInfoDAO.java
@@ -0,0 +1,142 @@
+package com.funnyChat.db;
+
+import java.awt.List;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PluginInfoDAO {
+	private Statement stmt;
+
+	public PluginInfoDAO() {
+		stmt = Conn.getInstance().getStmt();
+	}
+
+	public ArrayList<PluginInfo> find() {
+		try {
+			ArrayList<PluginInfo> pluginInfos = new ArrayList<PluginInfo>();
+			String sql = "select * from plugininfo";
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				PluginInfo pluginInfo = new PluginInfo();
+				pluginInfo.setPid(rs.getLong(1));
+				pluginInfo.setName(rs.getString(2));
+				pluginInfo.setUid(rs.getLong(3));
+				pluginInfo.setDescribe(rs.getString(4));
+				pluginInfo.setUptime(rs.getDate(5));
+				pluginInfo.setPath(rs.getString(6));
+				pluginInfos.add(pluginInfo);
+				System.out.println(pluginInfo.toString());
+			}
+			return pluginInfos;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public PluginInfo find(long _pid) {
+		try {
+			PluginInfo pluginInfo = new PluginInfo();
+			String sql = "select * from plugininfo where pid = " + _pid;
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				pluginInfo.setPid(_pid);
+				pluginInfo.setName(rs.getString(2));
+				pluginInfo.setUid(rs.getLong(3));
+				pluginInfo.setDescribe(rs.getString(4));
+				pluginInfo.setUptime(rs.getDate(5));
+				pluginInfo.setPath(rs.getString(6));
+			}
+			return pluginInfo;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public PluginInfo find(String _name) {
+		try {
+			PluginInfo pluginInfo = new PluginInfo();
+			String sql = "select * from plugininfo where name = '" + _name +"'";
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				pluginInfo.setPid(rs.getLong(1));
+				pluginInfo.setName(rs.getString(2));
+				pluginInfo.setUid(rs.getLong(3));
+				pluginInfo.setDescribe(rs.getString(4));
+				pluginInfo.setUptime(rs.getDate(5));
+				pluginInfo.setPath(rs.getString(6));
+			}
+			return pluginInfo;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	public boolean add(PluginInfo pluginInfo) {
+		try {
+			String sql = "insert into plugininfo(name,uid,describe,uptime,path) values('"
+			 +pluginInfo.getName()+"',"
+			 +pluginInfo.getUid()+",'"
+			 +pluginInfo.getDescribe()+"','"
+			 +pluginInfo.getUptime().toGMTString()+"','"
+			 +pluginInfo.getPath()+"')";
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	public boolean update(PluginInfo _pluginInfo) {
+		try {
+			String sql = "update plugininfo set name = '" +_pluginInfo.getName()+"',"
+			+"uid = "+_pluginInfo.getUid()+","
+			+"describe = '"+_pluginInfo.getDescribe()+"',"
+			+"uptime = '"+_pluginInfo.getUptime().toGMTString()+"',"
+			+"path = '"+_pluginInfo.getPath()+"' "
+			+"where pid = "+ _pluginInfo.getPid();
+			System.out.println(_pluginInfo.getPid());
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	public boolean delete(long _pid) {
+		try {
+			String sql = "delete from plugininfo where pid = "+_pid;
+			return stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void main(String[]args){
+		PluginInfoDAO pluginInfoDAO = new PluginInfoDAO();
+		pluginInfoDAO.find();
+		
+		PluginInfo pluginInfo = new PluginInfo();
+		pluginInfo.setUptime(new Date());
+		pluginInfo.setName("name5");
+		pluginInfo.setDescribe("wetrdrfg");
+		pluginInfo.setPath("sdgdhgdfs");
+		pluginInfo.setUid(1);
+		pluginInfoDAO.add(pluginInfo);
+		
+		pluginInfoDAO.find();
+		pluginInfo = pluginInfoDAO.find(pluginInfo.getName());
+		System.out.println("\n\n"+pluginInfo.toString());
+		pluginInfo.setDescribe("setDescribe");
+		pluginInfoDAO.update(pluginInfo);
+		
+		pluginInfoDAO.find();
+		
+		pluginInfoDAO.delete(pluginInfo.getPid());
+	}
+}
